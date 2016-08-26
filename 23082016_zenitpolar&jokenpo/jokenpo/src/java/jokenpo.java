b133831b8fb4086b1eda64331d47d97f94010974
Letícia Amaral <leticiatissa@gmail.com>
2016-08-25 22:22:38 -0300

    Add files via upload

--- 23082016_zenitpolar&jokenpo/jokenpo/src/java/jokenpo.java
+++ 23082016_zenitpolar&jokenpo/jokenpo/src/java/jokenpo.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Random;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,11 +10,6 @@ public class jokenpo extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            
-            String escolha = request.getParameter("escolha");
-            String escolhapc = "pedra";
-            //String escolhapc = (String)random.random(escolhapc);
-            
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
@@ -21,34 +17,70 @@ public class jokenpo extends HttpServlet {
             out.println("</head>");
             out.println("<body align=\"center\">");
             out.println("<h2>JOGO JOKENPO</h2>");
+            
+            String escolha = request.getParameter("choice");
+            
+            String[] choice = {"pedra", "papel", "tesoura"};
+            int sorteio = new Random().nextInt(3);
+            String escolhapc = choice[sorteio];
+            
             out.println("<b>Sua escolha: </b>" + escolha + "</br></br>");
             out.println("<b>Escolha do computador: </b>" + escolhapc + "</br></br>");
+            
+            boolean userwin = false;
             out.println("<b>Resultado: </b>");
-                if (escolha.trim().equals("papel") && escolhapc.trim().equals("papel")){ 
-                out.println("Empate!"); 
-                } else if (escolha.trim().equals("pedra") && escolhapc.trim().equals("pedra")){ 
+            
+            if (escolha.equals("papel")){
+                if(escolhapc.equals("papel")){
+                    userwin = true;
                     out.println("Empate!"); 
-                } else if (escolha.trim().equals("tesoura") && escolhapc.trim().equals("tesoura")){ 
+                }}
+            if (escolha.equals("pedra")){
+                if (escolhapc.equals("pedra")){
+                    userwin = true;
                     out.println("Empate!"); 
-                } else if (escolha.trim().equals("papel") && escolhapc.trim().equals("pedra")){ 
+                }}
+            if (escolha.equals("tesoura")){
+                if (escolhapc.equals("tesoura")){
+                    userwin = true;
+                    out.println("Empate!"); 
+                }}
+            if (escolha.equals("papel")){
+                if (escolhapc.equals("pedra")){
+                    userwin = true;
                     out.println("Você perdeu!<br>Pedra perde para Papel."); 
-                } else if (escolha.trim().equals("papel") && escolhapc.trim().equals("tesoura")){ 
+                }}
+            if (escolha.equals("papel")){
+                if (escolhapc.equals("tesoura")){
+                    userwin = true;
                     out.println("Você ganhou!<br>Tesoura perde para Pedra."); 
-                } else if (escolha.trim().equals("pedra") && escolhapc.trim().equals("papel")){ 
+                }}
+            if (escolha.equals("pedra")){
+                if (escolhapc.equals("papel")){
+                    userwin = true;
                     out.println("Você ganhou!<br>Pedra perde para Papel."); 
-                } else if (escolha.trim().equals("pedra") && escolhapc.trim().equals("tesoura")){ 
+                }}
+            if (escolha.equals("pedra")){
+                if (escolhapc.equals("tesoura")){
+                    userwin = true;
                     out.println("Você perdeu!<br>Papel perde para Tesoura."); 
-                } else if (escolha.trim().equals("tesoura") && escolhapc.trim().equals("papel")){ 
+                }}
+            if (escolha.equals("tesoura")){
+                if (escolhapc.equals("papel")){
+                    userwin = true;
                     out.println("Você perdeu!<br>Tesoura perde para Pedra."); 
-                } else if (escolha.trim().equals("tesoura") && escolhapc.trim().equals("pedra")){ 
+                }}
+            if (escolha.equals("tesoura")){
+                if (escolhapc.equals("pedra")){
+                    userwin = true;
                     out.println("Você ganhou!<br>Papel perde para Tesoura."); 
                 } else { 
-                    out.println("Escolha entre Pedra, Papel ou Tesoura"); 
+                    out.println("Escolha entre Pedra, Papel ou Tesoura");
                 }
-            out.println("</body>");
-            out.println("</html>");
+            }
         }
     }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
